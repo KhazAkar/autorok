ca2295fe45199b22c552b973656f396fa98cefe7
Damian Zaręba <damianzrb@zohomail.eu>
2022-01-12 03:12:40 +0100

    WIP on logging, it will be total custom solution

--- autorok/log.py
+++ autorok/log.py
@@ -1,17 +1,27 @@
+from pathlib import Path
 import typing
-import logging
-import pathlib
 import configparser
+import enum
 
+class Destination(enum.Enum):
+    STDOUT = "stdout"
+    FILE = "file"
 
 class Log:
     def __init__(self):
         self.status: bool = None
+        self.output_dst: Destination = None 
         self.live_cfg: typing.Dict[str] = {}
-        self.file_cfg: pathlib.Path = pathlib.Path()
+        self.file_cfg: Path = Path()
         self.__cfg_parser = configparser.ConfigParser()
 
+    def __preconfigure(self):
+        self.file_cfg = Path('.')
+        self.live_cfg['LogPath']= self.file_cfg
+        self.live_cfg['OutputDst'] = Destination.STDOUT.value 
+
     def enable_logging(self):
+        self.__preconfigure()
         self.status = True
         return self.status
 
--- tests/test_log.py
+++ tests/test_log.py
@@ -17,3 +17,21 @@ def test_should_disable_logging():
     log.disable_logging()
     assert log.status == False
     del log
+
+def test_should_provide_default_configuration():
+    log = Log()
+    log.enable_logging()
+    assert len(log.live_cfg) > 1
+    del log
+
+#def test_should_load_config_from_file():
+#    pass
+#
+#def test_should_configure_onthefly():
+#    pass
+#
+#def test_should_allow_configure_msg_format():
+#    pass
+#
+#def test_should_allow_configure_log_output_dst():
+#    pass

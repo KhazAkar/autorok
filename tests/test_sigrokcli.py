88d367e895aca63646bf356f1148189ff56f89ba
Damian Zaręba <damianzrb@zohomail.eu>
2022-01-22 21:13:00 +0100

    Make use of pytest fixture

--- tests/test_sigrokcli.py
+++ tests/test_sigrokcli.py
@@ -2,33 +2,36 @@ import pytest
 from autorok.autorok import Autorok, SigrokInterface
 from autorok.devices import device_map
 
-def test_should_scan_for_devices():
-    sigrok = Autorok(iface = SigrokInterface.SIGROK_CLI)
-    device_list = sigrok.scan_devices()
-    assert device_map.get('demo') in device_list 
+@pytest.fixture
+def sigrok_setup():
+    sigrok = Autorok(iface=SigrokInterface.SIGROK_CLI)
+    yield sigrok
     del sigrok
 
-def test_should_allow_selecting_device():
-    sigrok = Autorok(iface = SigrokInterface.SIGROK_CLI)
-    device_list = sigrok.scan_devices()
-    sigrok.select_device(device_list[0])
-    assert sigrok.active_device == device_list[0]
-    del sigrok
+def test_should_scan_for_devices(sigrok_setup):
+    device_list = sigrok_setup.scan_devices()
+    assert device_map.get('demo') in device_list
 
-def test_should_allow_reconfiguration_of_active_device():
-    sigrok = Autorok(iface = SigrokInterface.SIGROK_CLI)
-    device_list = sigrok.scan_devices()
-    sigrok.select_device(device_list[0])
-    sigrok.configure_analog_channels(["A0", "A1"])
-    sigrok.configure_digital_channels(["D2", "D4"])
-    assert sigrok.active_digital_channels == ['D2', 'D4']
-    assert sigrok.active_analog_channels == ['A0', 'A1']
 
-#def test_should_provide_measurement_of_desired_signal():
+def test_should_allow_selecting_device(sigrok_setup):
+    device_list = sigrok_setup.scan_devices()
+    sigrok_setup.select_device(device_list[0])
+    assert sigrok_setup.active_device == device_list[0]
+
+
+def test_should_allow_reconfiguration_of_active_device(sigrok_setup):
+    device_list = sigrok_setup.scan_devices()
+    sigrok_setup.select_device(device_list[0])
+    sigrok_setup.configure_analog_channels(["A0", "A1"])
+    sigrok_setup.configure_digital_channels(["D2", "D4"])
+    assert sigrok_setup.active_digital_channels == ['D2', 'D4']
+    assert sigrok_setup.active_analog_channels == ['A0', 'A1']
+
+# def test_should_provide_measurement_of_desired_signal():
 #    pass
 #
-#def test_should_provide_measurement_of_multiple_signals():
+# def test_should_provide_measurement_of_multiple_signals():
 #    pass
 #
-#def test_should_provide_signal_decoding():
+# def test_should_provide_signal_decoding():
 #    pass

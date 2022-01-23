7a8faf182fc25bc45492f958a953f1b3462f5b13
Damian Zaręba <damianzrb@zohomail.eu>
2022-01-23 22:35:49 +0100

    Create Output, Input and transform types

--- autorok/common.py
+++ autorok/common.py
@@ -1,7 +1,39 @@
 import pathlib
+import enum
 import typing
 from abc import ABC, abstractmethod
 from autorok.devices import Device
+
+class TransformModules(enum.Enum):
+    INVERT = 'invert'
+    NOP = 'nop'
+    SCALE = 'scale'
+
+class InputType(enum.Enum):
+    BINARY = 'binary'
+    CHRONOVU = 'chronovu'
+    CSV = 'csv'
+    LOGICPORT = 'logicport'
+    NULL = 'null'
+    RAW_ANALOG = 'raw_analog'
+    TRACE32 = 'trace32_ad'
+    VALUE_CHANGE_DUMP = 'vcd'
+    WAV = 'wav'
+
+class OutputType(enum.Enum):
+    CSV = 'csv'
+    ANALOG_ASCII = 'analog'
+    ASCII = 'ascii'
+    BINARY = 'binary'
+    BITS = 'bits'
+    CHRONOVU = 'chronovu-la8'
+    HEX = 'hex'
+    NULL = 'null'
+    OLS = 'ols'
+    SIGROK_ZIP = 'srzip'
+    VALUE_CHANGE_DUMP = 'vcd'
+    WAV = 'wav'
+    WAVEDROM = 'wavedrom'
 
 
 class SigrokDriver(ABC):

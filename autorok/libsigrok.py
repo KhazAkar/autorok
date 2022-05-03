dfba940264389bc91cdc37ca3112f56e59923682
Damian Zaręba <damianzrb@zohomail.eu>
2022-05-04 00:43:49 +0200

    Add placeholder for DSL variant of libsigrok

--- autorok/libsigrok.py
+++ autorok/libsigrok.py
@@ -1,5 +1,11 @@
 from autorok.common import SigrokDriver
 
+
 class LibSigrok(SigrokDriver):
     """ Driver using libsigrok instead of CLI """
+    pass
+
+
+class LibSigrok4DSL(SigrokDriver):
+    """ Driver using libsigrok instead of CLI, DSL variant """
     pass
